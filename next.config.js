9cffef724b4165b56bd52a0bcfffbf9dc152e21d
Andy Burris <andbapps@gmail.com>
2024-01-24 18:05:22 -0500

    add /c redirect to /customize for qr purposes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    async redirects() {
+        return [
+            {
+                source: "/c/:hash",
+                destination: "/customize/:hash",
+                permanent: true,
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
