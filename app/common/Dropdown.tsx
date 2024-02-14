82886a78094d5d569ba872922056e6b515e3246e
Andy Burris <andbapps@gmail.com>
2024-02-13 20:42:24 -0500

    fix refresh on menu click

--- app/common/Dropdown.tsx
+++ app/common/Dropdown.tsx
@@ -24,21 +24,29 @@ export function Dropdown({ trigger, menuItems }: { trigger: React.ReactNode, men
                         asChild={item.linkPath !== undefined}
                         className="outline-none"
                     >
-                        <Link 
-                            href={item.linkPath ?? ""}
-                            className={"flex gap-3 px-4 py-3 items-center" + ((item.onClick === undefined && item.linkPath === undefined) ? " cursor-default" : " hover:bg-neutral-100 focus:bg-neutral-100 cursor-pointer")}
-                            >
-                            <div className="text-2xl text-neutral-500">
-                                {item.icon}
-                            </div>
-                            <p className={`` + ((item.onClick === undefined && item.linkPath === undefined) ? "text-neutral-500" : "")}>
-                                {item.title}
-                            </p>
-                        </Link>
+                        { item.linkPath &&
+                            <Link href={item.linkPath}><ItemContent item={item}/></Link>
+                        }
+                        { item.linkPath === undefined &&
+                            <ItemContent item={item}/>
+                        }
                     </DropdownMenu.Item>
                 )}
             </DropdownMenu.Content>
         </DropdownMenu.Portal>
     </DropdownMenu.Root>
+    )
+}
+
+function ItemContent({ item }: { item: DropdownItem }) {
+    return (
+        <div className={"flex gap-3 px-4 py-3 items-center" + ((item.onClick === undefined && item.linkPath === undefined) ? " cursor-default" : " hover:bg-neutral-100 focus:bg-neutral-100 cursor-pointer")}>
+            <div className="text-2xl text-neutral-500">
+                {item.icon}
+            </div>
+            <p className={`` + ((item.onClick === undefined && item.linkPath === undefined) ? "text-neutral-500" : "")}>
+                {item.title}
+            </p>
+        </div>
     )
 }
